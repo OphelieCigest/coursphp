44f0938889207833c46064f557534234441baa32
Ophelie <ophelie.lahaye@cigest-group.fr>
2022-08-30 15:01:55 +0200

    fonctions multiplier é nomùbres

---
+++ exos1.php
@@ -0,0 +1,12 @@
+//Rappel
+
+//ON VA CREER UNE FONCTION QUI TRANSFORME LES STRING EN NUMBER
+//EXAMPLES"/
+////1234" --> 1234
+//"605"  --> 605
+//"1405" --> 1405
+//"-7" --> -7
+
+function stringToNumber($str) {
+    return (int) $str;
+}
